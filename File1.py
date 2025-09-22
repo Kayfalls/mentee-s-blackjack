70db8f4cba53c45f29a3094da572c462fd2c39af
Kabelo Maile <157735116+Kayfalls@users.noreply.github.com>
2025-09-22 14:56:28 +0200

    the loop for player finishing turn

--- File1.py
+++ File1.py
@@ -32,6 +32,38 @@ hand_active = False
 outcome = 0
 add_score = False
 results = ['', 'PLAYER BUSTED :(', 'Player WINS! :)', 'DEALER WINS :(', 'TIE GAME...']
+
+#deal cards by selecting randomly from dek,and make funtion for one card at a time
+def deal_cards(current_hand, current_deck):
+  card = random.randint(0, len(current_deck))
+  current_hand.append(current_deck[card - 1])
+  current_deck.pop(card - 1)
+  return current_hand, current_deck
+
+#draw scores for player and dealer on screen
+def draw_scores(player, dealer):
+  screen.blit(font.render(f'score [{player}]', True, 'white'), (350, 400))
+  if reveal_dealer:
+  screen.blit(font.render(f'score[{dealer}]', True, 'white'), (350, 100))
+
+#draw cards visually onto screen
+def draw_cards (player, dealer,reveal):
+  for i in range(len(player)):
+    pygame.draw.rect(screen, "white", [70 + (70 * i), 460 + (5 * i), 120, 220]. 0,5)
+    screen.blit(font.render(player[i], True, 'black'), (75 + 70 * i, 465 + 5 * i))
+    screen.blit(font.render(player[i], True, 'black'), (75 + 70 * i, 465 + 5 * i))
+    pygame.draw.rect(screen, 'red', [70 + (70 * i), 460 + (5 * i), 120, 220], 5, 5)
+    
+#if player hasn't finished turn, dealer will hide one card
+for i in range(len(dealer)):
+  pygame.draw.rect(screen, 'white', [70 + (70 * i), 160 + (5 * i), 120, 220], 0, 5)
+  if i != 0 or reveal:
+    screen.blit(font.render(dealer[i], True, 'black'), (75 + 70 * i, 165 + 5 * i))
+    screen.blit(font.render(dealer[i], True, 'black'), (75 + 70 * i , 335 + 5 * i))
+  else:
+    screen.blit(font.render('???', True, 'black'), (75 + 70 * i, 165 + 5 * i))
+    screen.blit(font.render('???', True, 'black'), (75 + 70 * i , 335 + 5 * i))
+  pygame.draw.rect(screen, 'blue', [70 + (70 * i), 160 + (5 * i), 120, 220], 5, 5)
 # •create a class cards
 # •create 50 card items and check if there isnt a module specifically for cards 
 # •randomise a set of cards before dealing
